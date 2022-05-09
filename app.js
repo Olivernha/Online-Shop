8e442e412f67e6aa2de6351b25235eeba14bf3c7
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-05-09 14:25:11 +0630

    adding ejs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express=require('express');
-const authRoutes = require('./routes/auth.routes')
+const path = require("path");
+const express = require("express");
+const authRoutes = require("./routes/auth.routes");
 const app = express();
-
-app.use(authRoutes)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(authRoutes);
 app.listen(3000);
