044465f117fc5a5e43b8d46dbad1ef2def147f0b
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-05-10 22:30:58 +0630

    adding globle error handling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const csrf = require("csurf");
 require("dotenv").config();
 const db = require("./database/database");
 const addCsrfTokenMiddleware= require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 const authRoutes = require("./routes/auth.routes");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(csrf());
 app.use(addCsrfTokenMiddleware);
 app.use(authRoutes);
-
+app.use(errorHandlerMiddleware);
 db.connectToDatabase().then(()=>{
     app.listen(port);
 }).catch(err=>{
