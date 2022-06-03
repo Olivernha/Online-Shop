adfeb81e1385e71fdac2d08f8c69f6937addcf5d
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-06-03 17:07:37 +0630

    fixing the bug

--- util/detachFile.js
+++ util/detachFile.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 function deleteFile(filename) {
-  console.log(filename);
-  return fs.unlinkSync(path.join("product-data", "images", filename));
+
+  return fs.unlinkSync(path.join(__dirname, "..", "product-data", 'images',filename));
 }
 module.exports = deleteFile;
